93d876c9e56520b268a0e6fff8f53dff57a5225a
Sarah Buddle <sarah.buddle@btinternet.com>
2021-02-14 14:34:29 +0000

    Comment out test statements

--- workflow/scripts/topgo_gentable.R
+++ workflow/scripts/topgo_gentable.R
@@ -7,25 +7,26 @@ setwd("~/OneDrive/Documents/Uni/III/Project/github/celegans-polyA/workflow")
 library(topGO)
 
 # Load GOdata
-# load('output/topgo_data/bristol/bristol_as_op50.RData')
+#load('output/topgo_data/altadena/altadena_as_op50.RData')
 load(snakemake@input$topgo_data)
 
 # Load topgo_results
-# load('output/topgo_result/classic/fisher/bristol/bristol_as_op50.RData')
+#load('output/topgo_result/classic/fisher/altadena/altadena_as_op50.RData')
 load(snakemake@input$topgo_result_classic_fisher)
 topgo_result_classic_fisher <- topgo_result
 
-# load('output/topgo_result/classic/ks/bristol/bristol_as_op50.RData')
+#load('output/topgo_result/classic/ks/altadena/altadena_as_op50.RData')
 load(snakemake@input$topgo_result_classic_ks)
 topgo_result_classic_ks <- topgo_result
 
-# load('output/topgo_result/elim/ks/bristol/bristol_as_op50.RData')
+#load('output/topgo_result/elim/ks/altadena/altadena_as_op50.RData')
 load(snakemake@input$topgo_result_elim_ks)
 topgo_result_elim_ks <- topgo_result
 
 all_res <- topGO::GenTable(GOdata,  classicFisher = topgo_result_classic_fisher,
                           classicKS = topgo_result_classic_ks,
                           elimKS = topgo_result_elim_ks,
+                          #orderBy = 'classicFisher',
                           orderBy = snakemake@wildcards$orderBy, 
                           ranksOf = "classicFisher", 
                           topNodes = 10)
