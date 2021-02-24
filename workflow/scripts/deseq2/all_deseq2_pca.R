4d410e96cdeac0ca73bdbe41cd1da16aabb5f616
Sarah Buddle <sarah.buddle@btinternet.com>
2021-02-24 16:07:45 +0000

    Add deseq2 scripts for comparison across locations

---
+++ workflow/scripts/deseq2/all_deseq2_pca.R
@@ -0,0 +1,49 @@
+#### PCA Plot to summarise differential expression analysis #### 
+
+# Set working directory
+setwd("~/OneDrive/Documents/Uni/III/Project/github/celegans-polyA/workflow")
+
+# Load packages
+library(DESeq2)
+library(ggplot2)
+
+# Load rlog_dds object made previously
+#vst_dds <- readRDS('output/deseq2_data/all/all_vst_dds.rds')
+vst_dds <- readRDS(snakemake@input$vst_dds)
+
+# Perform PCA analysis
+pcaData <- DESeq2::plotPCA(vst_dds, intgroup = c('diet', 'location'), returnData = TRUE)
+
+# Calculate percentage variation explained by each of the principle components
+percentVar <- round(100 * attr(pcaData, "percentVar"))
+
+# Legend
+
+# Reorder
+pcaData$diet <- factor(pcaData$diet, levels = c("as", "bp", "hb101", "op50", "pf", "m9"))
+
+# Labels
+diet_legend_labels <- c(expression(italic("Acinetobacter schindleri")),
+                   expression(italic("Bacillus pumilus")),
+                   expression(paste(italic("Escherichia coli"), " strain HB101")),
+                   expression(paste(italic("Escherichia coli"), " strain OP50")),
+                   expression(italic("Pseudomonas fragi")),
+                   "Starvation")
+
+location_legend_labels <- c('Altadena', 'Bristol')
+
+# Plot
+# tiff('output/deseq2_plots/all/all_deseq2_pca.tiff', width = 1600, height = 1000, res = 300)
+tiff(snakemake@output$pca_plot, width = 1600, height = 1000, res = 300)
+ggplot2::ggplot(pcaData) +
+  geom_point(mapping = aes(x = PC1, y = PC2, color = diet, shape = location), size = 2) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  guides(col=guide_legend("Diet"),
+         shape=guide_legend("Location")) +
+  theme(text = element_text(size=10)) + 
+  scale_color_brewer(labels = diet_legend_labels, palette = "Set2") +
+  scale_shape_manual(labels = location_legend_labels, values = c(16, 18)) +
+  theme_light()
+dev.off()
+
---
+++ workflow/scripts/deseq2/deseq2_all.R
@@ -0,0 +1,44 @@
+
+# Set working directory
+setwd('~/OneDrive/Documents/Uni/III/Project/github/celegans-polyA/workflow')
+
+# Load packages
+library(DESeq2)
+
+# Import sample info
+#samples <- as.data.frame(read.csv('scripts/deseq2/htseqcount_samples_full.csv'))
+samples <- as.data.frame(read.csv(snakemake@input$samples)) 
+  
+# Only include relevant locations
+samples <- subset(samples, location == 'bristol' | location == 'altadena')
+
+# Import count matrix
+#count_matrix <- readRDS('output/deseq2_data/all/count_matrix.rds')
+count_matrix <- readRDS(snakemake@input$count_matrix)
+
+# Remove rows with extra info rather than gene counts
+count_matrix <- subset(count_matrix, grepl('WBGene', rownames(count_matrix)))
+
+# Change NA to 0 or delete rows with NA - doesn't make any different to the PCA plot
+count_matrix[is.na(count_matrix)] <- 0
+#count_matrix <- na.omit(count_matrix)
+
+# Create deseq object
+full_dds <- DESeq2::DESeqDataSetFromMatrix(count_matrix, colData = samples, 
+                                           design = ~ diet + location)
+
+# Save resulting object
+saveRDS(full_dds, file = snakemake@output$full_dds)
+
+# Pre-filter data set to remove rows with 0 and 1 counts for all
+keep <- rowSums(counts(full_dds)) > 0
+dds <- full_dds[keep,]
+
+# Transform data to make sure it is homoskedastic
+# rlog better when n < 30, vst better if not
+vst_dds <- DESeq2::vst(dds, blind = FALSE)
+
+# Save rlog_dds object
+saveRDS(vst_dds, file = snakemake@output$vst_dds)
+
+
