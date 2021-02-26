79fa40f5c023e74d9db9e15bb18fd5487c1923d5
Sarah Buddle <sarah.buddle@btinternet.com>
2021-02-26 20:43:48 +0000

    Chnage to use .rds

--- workflow/scripts/annotations/granges.R
+++ workflow/scripts/annotations/granges.R
@@ -8,7 +8,7 @@ library(rtracklayer)
 
 # Import annotation
 # annotation <- rtracklayer::import('from_cluster/maker_annotations/trinity/bristol/as/rep2/trinity_bristol_as_rep2.gff')
-# annotation <- rtracklayer::import('from_cluster/liftover_annotations/bristol/liftover_bristol.gtf')
+# annotation <- rtracklayer::import('from_cluster/liftover_annotations/altadena/liftover_altadena.gtf')
 annotation <- rtracklayer::import(snakemake@input$annotation)
 
 # Remove rows corresponding to contigs
@@ -18,5 +18,5 @@ annotation <- annotation[which(annotation$type != 'contig'), ]
 annotation_genes <- annotation[which(annotation$type == 'gene'), ]
 
 # Save output
-save(annotation, file = snakemake@output$granges)
-save(annotation, file = snakemake@output$granges_genes)
+saveRDS(annotation, file = snakemake@output$granges)
+saveRDS(annotation_genes, file = snakemake@output$granges_genes)
