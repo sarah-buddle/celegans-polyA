e77f8cf72006320a57c887e4650b5f0e041f3793
Sarah Buddle <sarah.buddle@btinternet.com>
2021-02-26 20:29:47 +0000

    Tidy and use .rds

--- workflow/scripts/deseq2/deseq2.R
+++ workflow/scripts/deseq2/deseq2.R
@@ -1,22 +1,25 @@
 #### Creates DESeq2 objects for use in further analysis ####
 
 # Set working directory
-setwd("~/OneDrive/Documents/Uni/III/Project/github/celegans-polyA/workflow")
+setwd('~/OneDrive/Documents/Uni/III/Project/github/celegans-polyA/workflow')
 
 # Load packages
 library(DESeq2)
 
 # Import sample info
+# samples <- as.data.frame(read.csv('scripts/deseq2/htseqcount_samples_full.csv'))
 samples <- as.data.frame(read.csv(snakemake@input$samples))
 
 # Only include samples for specified location
+# samples <- subset(samples, location == 'bristol')
 samples <- subset(samples, location == snakemake@wildcards$location)
 
 # Make DESeqDataSet object from HTSeq-count data
-full_dds <- DESeq2::DESeqDataSetFromHTSeqCount(samples, "from_cluster/htseq_count_export", design = ~ diet)
+
+full_dds <- DESeq2::DESeqDataSetFromHTSeqCount(samples, 'from_cluster/htseq_count_export', design = ~ diet)
 
 # Save resulting object
-save(full_dds, file = snakemake@output$full_dds)
+saveRDS(full_dds, file = snakemake@output$full_dds)
 
 # Pre-filter data set to remove rows with 0 and 1 counts for all
 keep <- rowSums(counts(full_dds)) > 1
@@ -27,4 +30,4 @@ dds <- full_dds[keep,]
 rlog_dds <- DESeq2::rlog(dds, blind = FALSE)
 
 # Save rlog_dds object
-save(rlog_dds, file = snakemake@output$rlog_dds)
+saveRDS(rlog_dds, file = snakemake@output$rlog_dds)
