fed5806b35522d34e21d464a6e07012e480e34b4
Sarah Buddle <sarah.buddle@btinternet.com>
2021-04-22 11:02:16 +0100

    Remove unused environments

--- cluster_workflow/conda_environments_cluster.sh
+++ cluster_workflow/conda_environments_cluster.sh
@@ -1,3 +1,5 @@
+# Create all necessary conda environments for cluster workflow
+
 # samtools
 conda create --name samtools=1.11 --channel conda-forge --channel bioconda --yes samtools=1.11
 conda activate samtools=1.11
@@ -52,13 +54,6 @@ conda activate stringtie=2.1.4
 conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/stringtie=2.1.4.yaml
 conda deactivate
 
-# DESeq2
-conda create --name r-base=4.0.3_bioconductor-deseq2=1.30.0 --channel conda-forge --channel bioconda --yes \
-r-base=4.0.3 bioconductor-deseq2=1.30.0
-conda activate r-base=4.0.3_bioconductor-deseq2=1.30.0
-conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/r-base=4.0.3_bioconductor-deseq2=1.30.0.yaml
-conda deactivate
-
 # maker
 conda create --name maker=2.31.10 --channel conda-forge --channel bioconda --yes maker=2.31.10
 conda activate maker=2.31.10
@@ -69,12 +64,6 @@ conda deactivate
 conda create --name repeatmasker=4.1.1 --channel conda-forge --channel bioconda --yes repeatmasker=4.1.1
 conda activate repeatmasker=4.1.1
 conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/repeatmasker=4.1.1.yaml
-conda deactivate
-
-# blast (for windowmasker)
-conda create --name blast=2.10.1 --channel conda-forge --channel bioconda --yes blast=2.10.1
-conda activate blast=2.10.1
-conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/blast=2.10.1.yaml
 conda deactivate
 
 # htseq-count
@@ -89,32 +78,8 @@ conda activate gffcompare=0.11.2
 conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/gffcompare=0.11.2.yaml
 conda deactivate
 
-# rclone (first two lines also on local machine)
-conda create --name rclone=1.53.1 --channel conda-forge --yes rclone=1.53.1
-conda activate rclone=1.53.1
-conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/rclone=1.53.1.yaml
-conda deactivate
-
 # soapdenovo-trans
 conda create --name soapdenovo-trans=1.04 --channel conda-forge --channel bioconda --yes soapdenovo-trans=1.04
 conda activate soapdenovo-trans=1.04
 conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/soapdenovo-trans=1.04.yaml
-conda deactivate
-
-# rseqc
-conda create --name rseqc=4.0.0 --channel conda-forge --channel bioconda --yes rseqc=4.0.0
-conda activate rseqc=4.0.0
-conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/rseqc=4.0.0.yaml
-conda deactivate
-
-# velvet
-conda create --name velvet=1.2.10 --channel conda-forge --channel bioconda --yes velvet=1.2.10
-conda activate velvet=1.2.10
-conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/velvet=1.2.10.yaml
-conda deactivate
-
-# bbtools
-conda create --name bbtools=37.62 --channel agbiome --yes bbtools=37.62
-conda activate bbtools=37.62
-conda env export --no-builds | sed '$d' | sed '$d' > envs/conda/bbtools=37.62.yaml
 conda deactivate
