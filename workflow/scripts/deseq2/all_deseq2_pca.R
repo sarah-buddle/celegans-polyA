1f417a80992f4610c20a5f87b609019a9a7b4cf2
Sarah Buddle <sarah.buddle@btinternet.com>
2021-04-22 14:54:41 +0100

    Change plot labels

--- workflow/scripts/deseq2/all_deseq2_pca.R
+++ workflow/scripts/deseq2/all_deseq2_pca.R
@@ -1,8 +1,5 @@
 #### PCA Plot to summarise differential expression analysis #### 
 
-# Set working directory
-setwd("~/OneDrive/Documents/Uni/III/Project/github/celegans-polyA/workflow")
-
 # Load packages
 library(DESeq2)
 library(ggplot2)
@@ -22,6 +19,8 @@ percentVar <- round(100 * attr(pcaData, "percentVar"))
 # Reorder
 pcaData$diet <- factor(pcaData$diet, levels = c("as", "bp", "hb101", "op50", "pf", "m9"))
 
+pcaData$location <- factor(pcaData$location, levels = c('bristol', 'altadena'))
+
 # Labels
 diet_legend_labels <- c(expression(italic("Acinetobacter schindleri")),
                    expression(italic("Bacillus pumilus")),
@@ -30,20 +29,22 @@ diet_legend_labels <- c(expression(italic("Acinetobacter schindleri")),
                    expression(italic("Pseudomonas fragi")),
                    "Starvation")
 
-location_legend_labels <- c('Altadena', 'Bristol')
+location_legend_labels <- c('N2', 'PS2025')
 
 # Plot
 # tiff('output/deseq2_plots/all/all_deseq2_pca.tiff', width = 1600, height = 1000, res = 300)
-tiff(snakemake@output$pca_plot, width = 1600, height = 1000, res = 300)
 ggplot2::ggplot(pcaData) +
   geom_point(mapping = aes(x = PC1, y = PC2, color = diet, shape = location), size = 2) +
   xlab(paste0("PC1: ", percentVar[1], "% variance")) +
   ylab(paste0("PC2: ", percentVar[2], "% variance")) +
   guides(col=guide_legend("Diet"),
-         shape=guide_legend("Location")) +
-  theme(text = element_text(size=10)) + 
+         shape=guide_legend("Strain")) +
   scale_color_brewer(labels = diet_legend_labels, palette = "Set2") +
   scale_shape_manual(labels = location_legend_labels, values = c(16, 18)) +
-  theme_light()
-dev.off()
+  theme_light() +
+  theme(text = element_text(size = 10), panel.grid = element_blank()) +
+  # guides(shape = FALSE) +
+  ggtitle('All')
 
+ggsave(filename = snakemake@output$pca_plot, device = 'tiff', width = 15,
+       height = 7.5, units = 'cm', dpi = 320)
