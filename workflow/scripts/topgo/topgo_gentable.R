d097495ed92423fa670657b5a8ba121c650447f4
Sarah Buddle <sarah.buddle@btinternet.com>
2021-04-22 16:18:23 +0100

    Use rds rather than RData

--- workflow/scripts/topgo/topgo_gentable.R
+++ workflow/scripts/topgo/topgo_gentable.R
@@ -1,34 +1,28 @@
 #### Generate table of most represented classes of differentially expressed genes ####
 
-# Set working directory
-setwd("~/OneDrive/Documents/Uni/III/Project/github/celegans-polyA/workflow")
-
 # Load packages
 library(topGO)
 
 # Load GOdata
 #load('output/topgo_data/altadena/altadena_as_op50.RData')
-load(snakemake@input$topgo_data)
+topgo_data <- readRDS(snakemake@input$topgo_data)
 
 # Load topgo_results
 #load('output/topgo_result/classic/fisher/altadena/altadena_as_op50.RData')
-load(snakemake@input$topgo_result_classic_fisher)
-topgo_result_classic_fisher <- topgo_result
+topgo_result_classic_fisher <- readRDS(snakemake@input$topgo_result_classic_fisher)
 
 #load('output/topgo_result/classic/ks/altadena/altadena_as_op50.RData')
-load(snakemake@input$topgo_result_classic_ks)
-topgo_result_classic_ks <- topgo_result
+topgo_result_classic_ks <- readRDS(snakemake@input$topgo_result_classic_ks)
 
 #load('output/topgo_result/elim/ks/altadena/altadena_as_op50.RData')
-load(snakemake@input$topgo_result_elim_ks)
-topgo_result_elim_ks <- topgo_result
+topgo_result_elim_ks <-readRDS(snakemake@input$topgo_result_elim_ks)
 
-all_res <- topGO::GenTable(GOdata,  classicFisher = topgo_result_classic_fisher,
+all_res <- topGO::GenTable(topgo_data,  classicFisher = topgo_result_classic_fisher,
                           classicKS = topgo_result_classic_ks,
                           elimKS = topgo_result_elim_ks,
                           #orderBy = 'classicFisher',
-                          orderBy = snakemake@wildcards$orderBy, 
-                          ranksOf = "classicFisher", 
-                          topNodes = 10)
+                          orderBy = snakemake@wildcards$orderBy,
+                          ranksOf = "classicFisher",
+                          topNodes = 1000)
 
-save(all_res, file = snakemake@output$topgo_gentable)
+saveRDS(all_res, file = snakemake@output$topgo_gentable)
