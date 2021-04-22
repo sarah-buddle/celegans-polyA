504beaff0f3eb6297fd3e50eeaa74b8008532a7b
Sarah Buddle <sarah.buddle@btinternet.com>
2021-04-22 15:02:14 +0100

    Update comments

--- workflow/scripts/deseq2/de_analysis.R
+++ workflow/scripts/deseq2/de_analysis.R
@@ -1,20 +1,17 @@
-#### Differential expression analysis ####
-
-# set working directory
-setwd("~/OneDrive/Documents/Uni/III/Project/github/celegans-polyA/workflow")
+#### Differential expression analysis between two diets for same location ####
 
 # Load packages
 library(DESeq2)
 
 # Load dds_full made earlier
-load('output/deseq2_data/bristol/bristol_full_dds.RData')
-load(snakemake@input$full_dds)
+#load('output/deseq2_data/bristol/bristol_full_dds.RData')
+full_dds <- readRDS(snakemake@input$full_dds)
 
-# Perfrom differential expression analysis
+# Perform differential expression analysis
 ddsDE <- DESeq2::DESeq(full_dds)
-dds_res <- DESeq2::results(ddsDE, contrast = c("diet", 
-                                                snakemake@wildcards$diet1, 
+dds_res <- DESeq2::results(ddsDE, contrast = c('diet',
+                                                snakemake@wildcards$diet1,
                                                 snakemake@wildcards$diet2))
 
 # Save dds_res object
-save(dds_res, file = snakemake@output$dds_res)
+saveRDS(dds_res, file = snakemake@output$dds_res)
